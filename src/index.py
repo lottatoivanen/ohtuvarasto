60b6f70e37f36594b5e74421fca4229798c0c1dc
Lotta Toivanen <lotta.toivanen@helsinki.fi>
2025-11-08 21:51:45 +0200

    Add pylintrc and modify Python files to pass tests

--- src/index.py
+++ src/index.py
@@ -1,19 +1,17 @@
 from varasto import Varasto
 
-
-def main():
-    mehua = Varasto(100.0)
-    olutta = Varasto(100.0, 20.2)
-
+def alku_tilanne(mehua, olutta):
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
 
+def olut_getterit(olutta):
     print("Olut getterit:")
     print(f"saldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
 
+def test_mehu_setterit(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
@@ -22,6 +20,7 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
+def test_virhetilanne():
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
@@ -31,28 +30,50 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+
+def test_lisaa_liikaa(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
     print(f"Olutvarasto: {olutta}")
 
+def test_lisaa_negatiivinen(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.lisaa_varastoon(-666.0)")
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
 
+def test_ota_liikaa(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
     print(f"saatiin {saatiin}")
     print(f"Olutvarasto: {olutta}")
 
+def test_ota_negatiivinen(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.otaVarastosta(-32.9)")
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
 
+def test_kaikki(mehua, olutta):
+    alku_tilanne(mehua, olutta)
+    olut_getterit(olutta)
+    test_mehu_setterit(mehua)
+    test_virhetilanne()
+    test_lisaa_liikaa(olutta)
+    test_lisaa_negatiivinen(mehua)
+    test_ota_liikaa(olutta)
+    test_ota_negatiivinen(mehua)
+
+
+
+def main():
+    mehua = Varasto(100.0)
+    olutta = Varasto(100.0, 20.2)
+
+    test_kaikki(mehua, olutta)
 
 if __name__ == "__main__":
     main()
